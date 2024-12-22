194cad9dbaa4b6d1dbc8b26669569dd4df902434
zhangshilong <3131521901@qq.com>
2024-12-22 14:35:19 +0800

    feat: upgrade vue

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { fileURLToPath, URL } from "node:url";
 
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfig } from "vite";
 import legacy from "@vitejs/plugin-legacy";
 import vue2 from "@vitejs/plugin-vue2";
 import vue2Jsx from "@vitejs/plugin-vue2-jsx";
